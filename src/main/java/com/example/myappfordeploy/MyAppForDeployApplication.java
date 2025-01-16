1464f5ef044bfe66279824280dbb6f4bc02f5f13
GurbanAlizada <qurban@mina.az>
2025-01-16 17:43:36 +0400

    aaa

--- src/main/java/com/example/myappfordeploy/MyAppForDeployApplication.java
+++ src/main/java/com/example/myappfordeploy/MyAppForDeployApplication.java
@@ -2,9 +2,20 @@ package com.example.myappfordeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping
 @SpringBootApplication
 public class MyAppForDeployApplication {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello 1";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyAppForDeployApplication.class, args);
