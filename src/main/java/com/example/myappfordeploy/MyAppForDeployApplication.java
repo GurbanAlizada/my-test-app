0adc93a0052a05588b7f9afddc7e69861bac4354
GurbanAlizada <qurban@mina.az>
2025-01-16 17:02:52 +0400

    first commit

---
+++ src/main/java/com/example/myappfordeploy/MyAppForDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.myappfordeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppForDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppForDeployApplication.class, args);
+    }
+
+}
